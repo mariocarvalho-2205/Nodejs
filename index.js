53b368fc4b93943fb702ffe658b8e26b71f2982f
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2022-11-24 14:10:39 -0300

    nodejs

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    
+    res.statusCode = 200;
+    res.setHeader("content-type", "text/plain");
+
+    res.end('Hello Mário');
+})
+
+server.listen(port, hostname, () => {
+    console.log('Servidor Ok')
+})
